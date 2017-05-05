b4687cce973a85bf6db6c895271578adbc906477
liuniuliuniu <416997919@qq.com>
2017-05-05 13:29:38 +0800

    Complete-Version1.0

---
+++ LLActionSheetView/LLActionSheetView/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  LLActionSheetView
+//
+//  Created by liushaohua on 2017/5/5.
+//  Copyright © 2017年 liushaohua. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LLActionSheetView.h"
+
+@interface ViewController ()<LLActionSheetDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    
+}
+
+- (IBAction)actionSheetClick:(id)sender {
+    
+    NSArray *arr = @[@"相机",@"相册",@"保存图片"];
+    
+    LLActionSheetView *sheetV = [[LLActionSheetView alloc]initWithTitleArray:arr andShowCancel:YES];
+    sheetV.delegate = self;
+    
+    // block回调
+    sheetV.ClickIndex = ^(NSInteger index) {
+        NSLog(@"block--%zd",index);
+    };
+    [self.view.window addSubview:sheetV];
+}
+
+- (void)actionSheetView:(LLActionSheetView *)actionSheetView clickButtonAtIndex:(NSInteger)Index{
+    NSLog(@"delegate--%zd",Index);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
